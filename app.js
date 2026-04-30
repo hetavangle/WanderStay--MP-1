7d19b2da2f06de63c9ef0845ead6a520966deb19
Hetav Pai Angle <hetav.pai.angle@gmail.com>
2026-04-30 18:34:30 +0530

    added new route and edit route

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderstay";
 main()
@@ -31,7 +33,7 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", { allListings });
 });
 
-//new hotel adding route
+//new hotel adding  form route
 app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
 });
@@ -41,6 +43,21 @@ app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
+});
+
+//new hotel addition route
+app.post("/listings", async (req, res) => {
+  // let { title, decription, image, price, country } = req.body;
+  let newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//edit route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
 });
 
 // app.get("/testListing", async (req, res) => {
