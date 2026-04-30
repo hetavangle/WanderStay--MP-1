5cadd55aad32a3d11cd486a28c4e0bef2be78b7b
Hetav Pai Angle <hetav.pai.angle@gmail.com>
2026-04-30 23:29:53 +0530

    delete route addded

--- app.js
+++ app.js
@@ -60,6 +60,21 @@ app.get("/listings/:id/edit", async (req, res) => {
   res.render("listings/edit.ejs", { listing });
 });
 
+//update route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+//delete route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
 // app.get("/testListing", async (req, res) => {
 //   let samplelisting = new Listing({
 //     title: "My home villa",
