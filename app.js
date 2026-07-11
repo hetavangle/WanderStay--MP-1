5e4bfa79b9a9fda2cad6dd1750faf603640096b0
Hetav Pai Angle <hetav.pai.angle@gmail.com>
2026-07-11 12:34:12 +0530

    error handeling with joi added for server side handeling

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utility/wrapAsync.js");
 const ExpressError = require("./utility/ExpressError.js")
+const { listingSchema } = require("./schema.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -32,6 +33,17 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+
+  if (error) {
+    let errMsg = error.details.map((e) => e.message).join(",");
+    throw new ExpressError(400, error);
+  } else {
+    next();
+  }
+}
+
 //main route
 app.get("/listings", wrapAsync(async (req, res) => {
   const allListings = await Listing.find({});
@@ -51,10 +63,7 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 //new hotel addition route
-app.post("/listings", wrapAsync(async (req, res, next) => {
-  if (!req.body.listing) {
-    throw new ExpressError(400, "Send Valid data");
-  }
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
@@ -68,10 +77,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 //update route
-app.put("/listings/:id", wrapAsync(async (req, res) => {
-  if (!req.body.listing) {
-    throw new ExpressError(400, "Send Valid data");
-  }
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
