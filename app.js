a5f7e76e749600eb085cfe27183e75438e937789
Hetav Pai Angle <hetav.pai.angle@gmail.com>
2026-07-25 21:42:09 +0530

    add user authentication: integrate passport and local strategy for user login functionality

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utility/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js")
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -42,18 +44,26 @@ const sessionOptions = {
     httpOnly: true
   }
 }
-app.get("/", (req, res) => {
-  res.render("home.ejs");
-});
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 })
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
