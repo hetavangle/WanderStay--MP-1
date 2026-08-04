ade9ec2c7ac40a3afeaf18ba528daa2503ee3caa
Hetav Pai Angle <hetav.pai.angle@gmail.com>
2026-08-04 23:05:23 +0530

    added secret key in .env and accesed it from .env

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: "myferraricode",
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
@@ -55,7 +55,7 @@ store.on("error", (err) => {
 
 const sessionOptions = {
   store,
-  secret: "myferraricode",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
